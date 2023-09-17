33508d72063981984d738434d1974909d2aede8c
Slim <37602955+slal2003@users.noreply.github.com>
2023-09-17 14:41:23 +0200

    chnage eval function to handle close terms

--- app/helpers.py
+++ app/helpers.py
@@ -101,10 +101,10 @@ def generate_definition( term, domain, keywords):
     definition = llm(messages)
     return definition
 
-def generate_openai_definition( term, domain, keywords):
+def generate_openai_definition( term, domain, keywords, df):
     messages = [
-        # SystemMessage(content=f'You are a seasoned Data Stewart, expert in data analytics and business glossaries'),
-        HumanMessage(content=f'Suggest a definition for this term: {term} of this {domain} and these keywords {keywords} making sure the definition is concise, clear and elaborate enough to discriminate against other terms from this domain. return the answer in markdown format. It needs to be ready to populate the glossary. makle sure only the definition is output and not the rules, title or anything else')
+        SystemMessage(content=f'Your role is to output a definition text'),
+        HumanMessage(content=f'Suggest a definition for this term: {term} of this {domain} and these keywords {keywords} making sure the definition is concise, clear and elaborate enough to discriminate against these 4  closest terms already in the glossary: {df.head(4)} . \n Return the answer in markdown format. \n the definition needs to be ready to populate the glossary. \n the output need to contain only the definition no rules, no title or anything else \n Definition:')
     ]
     llm = ChatOpenAI(temperature=0.9, model_name="gpt-3.5-turbo-0613", max_tokens=200)
     print(llm)
@@ -112,19 +112,22 @@ def generate_openai_definition( term, domain, keywords):
     definition = llm(messages)
     return definition
 
-def generate_openai_evaluation(term, domain, keywords, definition):
+def generate_openai_evaluation(term, domain, keywords, definition, df):
         load_dotenv(find_dotenv())
-        messages = [
-            SystemMessage(content=f'You are responsible for our company glossary to make sure it only contains terms and definitions related to these terms that are unambiguous and clear so confusion and duplications are avoided'),
-            # HumanMessage(content=f'Evaluate the adequation of this business/data analytics {term} from this {domain} its keywords {keywords} and its GPT generated {definition} and suggest better term to avoid confusion and ambiguity. It is important to limit yourself to the evaluation only')
-            HumanMessage(content=f"""
-                         Unless the term {term} and the keywords {keywords} provided to build a perfect definition are clear and make sense in the {domain} domain, provide point form, markdown formatted improvement suggestions.
+        system_message=f'You are responsible for our company glossary to make sure it only contains terms and definitions related to these terms that are unambiguous and clear so confusion and duplications are avoided'
+        human_message=f"""
+                         Evalute how relevant the term {term} as well as its {keywords} taking into account these 4 closest terms and defintions {df.head(4)} and unless the term {term} and the keywords {keywords} provided to build a perfect definition are clear and make sense in the {domain} domain, provide point form, markdown formatted improvement suggestions. Pay a special attention to possible ambuigities with existing terms and you can suggest modifications to suggested terms if they are too vague. 
                          Here is the definition that GPT was able to provide based on the input he got: {definition} \n
                          Start by providing a score in percentage to denote your overall evaluation in the 1st line.
                          Then suggest a new better term if needed and make sure explaining why.
                          Finally provide a better definition if needed explaining why or suggest to the user to repeat his request but with providing more information to allow creation of a better definintion
                                                   
-                         """)
+                         """
+        print(human_message)
+        messages = [
+            SystemMessage(content=system_message),
+            # HumanMessage(content=f'Evaluate the adequation of this business/data analytics {term} from this {domain} its keywords {keywords} and its GPT generated {definition} and suggest better term to avoid confusion and ambiguity. It is important to limit yourself to the evaluation only')
+            HumanMessage(content=human_message)
         ]
         llm = ChatOpenAI(temperature=0.9, model_name="gpt-3.5-turbo-0613", max_tokens=1200)
         evaluation = llm(messages)
@@ -151,9 +154,12 @@ def generate_azure_evaluation(term, domain, keywords, definition):
 
 def generate_openai_evaluation2(term, domain, keywords, definition):
         load_dotenv(find_dotenv())
+        system_message = f'You are a seasoned Data Stewart, expert in data analytics and business glossaries'
+        human_message = f'Evaluate the adequation of this business/data analytics {term} from this {domain} its keywords {keywords} and its GPT generated {definition} and suggest better term to avoid confusion and ambiguity. It is important to limit yourself to the evaluation only'
+        print(human_message)
         messages = [
-            SystemMessage(content=f'You are a seasoned Data Stewart, expert in data analytics and business glossaries'),
-            HumanMessage(content=f'Evaluate the adequation of this business/data analytics {term} from this {domain} its keywords {keywords} and its GPT generated {definition} and suggest better term to avoid confusion and ambiguity. It is important to limit yourself to the evaluation only')
+            SystemMessage(content=system_message),
+            HumanMessage(content=human_message)
         ]
         llm = ChatOpenAI(temperature=0.9, model_name="gpt-4-0613", max_tokens=200)
         evaluation = llm(messages)
